298552351c07b881826bedf4458060d52fb438a9
Catalina Galan <catalina.galan@hotmail.com>
2023-09-02 19:08:16 -0400

    corrected and refactored

--- 01-fundamentals/projects/password_checker.py
+++ 01-fundamentals/projects/password_checker.py
@@ -5,9 +5,6 @@ NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 PASSWORDS = ['helloWorld', 'imcisamazing69', 'ilovecookies!23', 'python1s@mazingLanguage', 'hola3#']
 
-HAS_SPECIAL_CHAR = False
-HAS_NUMBER = False
-HAS_7_CHARS = False
 
 # Write a program that checks which of the passwords inside PASSWORDS meet the 3 following criteria:
 # 1. Minimum characters of 7
@@ -16,9 +13,11 @@ HAS_7_CHARS = False
 # Tip: You may want to loop over PASSWORDS and use multiple if conditions
 
 for password in PASSWORDS:
-    if len(password) < 7:
-        HAS_7_CHARS = False
-    else:
+    HAS_SPECIAL_CHAR = False
+    HAS_NUMBER = False
+    HAS_7_CHARS = False
+
+    if len(password) >= MIN_CHARACTERS:
         HAS_7_CHARS = True
 
     for number in NUMBERS:
@@ -31,7 +30,7 @@ for password in PASSWORDS:
             HAS_SPECIAL_CHAR = True
             break
 
-    if (HAS_7_CHARS == True) and (HAS_SPECIAL_CHAR == True) and (HAS_NUMBER == True):
-        print(f"{password} is a valid password")
+    if HAS_7_CHARS and HAS_SPECIAL_CHAR and HAS_NUMBER:
+        print(f"{password} is a valid password {HAS_7_CHARS}")
     else:
         print(f"{password} is NOT a valid password")
