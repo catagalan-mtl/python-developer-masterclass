94b9d780753fe1dac10ce9264062a81ef8512b87
Catalina Galan <catalina.galan@hotmail.com>
2023-09-02 18:57:03 -0400

    challenge solved

--- 01-fundamentals/projects/password_checker.py
+++ 01-fundamentals/projects/password_checker.py
@@ -3,7 +3,7 @@ MIN_CHARACTERS = 7
 SPECIAL_CHARS = ['!', '?', '#', '@', '$', '*']
 NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-PASSWORDS = ['helloWorld', 'imcisamazing69', 'ilovecookies!23', 'python1s@mazingLanguage']
+PASSWORDS = ['helloWorld', 'imcisamazing69', 'ilovecookies!23', 'python1s@mazingLanguage', 'hola3#']
 
 HAS_SPECIAL_CHAR = False
 HAS_NUMBER = False
@@ -15,15 +15,23 @@ HAS_7_CHARS = False
 # 3. Must include at least one number
 # Tip: You may want to loop over PASSWORDS and use multiple if conditions
 
-def check_length(password):
-    if len(password) < 7:
-        print(f"{password} is not a valid password")
-    elif len(password) >= 7:
-        return 1
-
-def check_special(password):
-
-
 for password in PASSWORDS:
-    if check_length(password):
+    if len(password) < 7:
+        HAS_7_CHARS = False
+    else:
+        HAS_7_CHARS = True
+
+    for number in NUMBERS:
+        if str(number) in password:
+            HAS_NUMBER = True
+            break
+
+    for special in SPECIAL_CHARS:
+        if special in password:
+            HAS_SPECIAL_CHAR = True
+            break
+
+    if (HAS_7_CHARS == True) and (HAS_SPECIAL_CHAR == True) and (HAS_NUMBER == True):
         print(f"{password} is a valid password")
+    else:
+        print(f"{password} is NOT a valid password")
