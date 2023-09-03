5e2c4fe93e45d8e4f919ad4395bfdb251a8d37bd
Catalina Galan <catalina.galan@hotmail.com>
2023-09-03 14:31:31 -0400

    reworked using try, except

--- 01-fundamentals/exercises/areas.py
+++ 01-fundamentals/exercises/areas.py
@@ -13,22 +13,41 @@ def circle_area(r):
     return round(area, 2)
 
 def main():
-    shape = input("What shape do you want to calculate?\n>").lower()
+    while True:
+        shape = input("What shape do you want to calculate?\n>").lower()
+        if (shape == "circle" or shape == "rectangle" or shape == "triangle"):
+            break
+        else:
+            print(f"{shape.capitalize()} is not a valid shape\n")
 
     if shape == "rectangle":
-        x = int(input("What's the lenght of the first side?\n>"))
-        y = int(input("What's the lenght of the second side?\n>"))
-        area = rectangle_area(x, y)
-    elif shape == "triangle":
-        x = int(input("what's the base of the triangle?\n>"))
-        y = int(input("what's the height of the triangle?\n>"))
-        area = triangle_area(x, y)
-    elif shape == "circle":
-        x = int(input("what's the radius of the circle?\n>"))
-        area = circle_area(x)
-    else:
-        print(f"{shape.capitalize()} is not a valid shape")
-        return 1
+        while True:
+            try:
+                x = int(input("What's the lenght of the first side?\n>"))
+                y = int(input("What's the lenght of the second side?\n>"))
+                area = rectangle_area(x, y)
+                break
+            except(ValueError):
+                print("Error: non numeric value\n")
+
+    if shape == "triangle":
+        while True:
+            try:
+                x = int(input("what's the base of the triangle?\n>"))
+                y = int(input("what's the height of the triangle?\n>"))
+                area = triangle_area(x, y)
+                break
+            except(ValueError):
+                print("Error: non numeric value\n")
+
+    if shape == "circle":
+        while True:
+            try:
+                x = int(input("what's the radius of the circle?\n>"))
+                area = circle_area(x)
+                break
+            except(ValueError):
+                print("Error: non numeric value\n")
 
     print(f"The area of the {shape} is {area}\n---")
     return 0
